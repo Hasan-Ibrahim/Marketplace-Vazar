0bafda8426d8ffcc005339673c8401f3e436f32e
Shuvo <shuvoiit0208@gmail.com>
2015-01-13 20:53:01 +0600

    client side testing in progress

---
+++ Web/Scripts/App/Account/Controllers/registrationController.js
@@ -0,0 +1,7 @@
+﻿appModule.controller('registrationController', ['$scope','accountService',
+    function ($scope, accountService) {
+        $scope.register = function() {
+            accountService.register($scope.registerModel);
+        }
+    }
+]);
