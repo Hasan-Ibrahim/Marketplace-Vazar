e6706630aa7729e8c4e232309b3a0ca753b3b105
Shuvo <shuvoiit0208@gmail.com>
2015-01-13 20:53:01 +0600

    client side testing in progress

---
+++ Web/Scripts/Test/appTests.js
@@ -0,0 +1,39 @@
+﻿///<reference path="~/Scripts/Lib/jasmine.js"/>
+///<reference path="~/Scripts/Lib/angular.js"/>
+///<reference path="~/Scripts/Lib/angular-mocks.js"/>
+
+///<reference path="~/Scripts/App/Account/Controllers/loginController.js"/>
+///<reference path="~/Scripts/App/Account/Services/accountRepository.js"/>
+///<reference path="~/Scripts/App/Account/Services/accountService.js"/>
+
+describe("Controllers", function () {
+
+    beforeEach(angular.module("appModule", []));
+
+    describe("login controller", function () {
+
+        var scope,
+            accountService,
+            controller;
+
+        beforeEach(inject(function ($rootScope, $controller) {
+            scope = $rootScope.$new();
+
+            accountService = {
+                login: function (loginModel) {
+                    return true;
+                }
+            }
+
+            controller = $controller('loginController', { $scope: scope, accountService: accountService });
+        }));
+
+        it('the page should login', function () {
+            expect(true).toBe(true);
+        });
+
+        it('It should pass', function () {
+            expect(true).toBe(true);
+        });
+    });
+});
