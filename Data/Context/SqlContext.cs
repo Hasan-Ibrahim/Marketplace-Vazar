74a07f72c19b60efd6285f46ce899bc83356f1a2
tajkia <tajkia@localhost>
2015-01-17 16:18:41 +0600

    Database gateway added

---
+++ Data/Context/SqlContext.cs
@@ -0,0 +1,18 @@
+﻿using System.Configuration;
+using System.Data.Entity;
+using Data.Model;
+
+namespace Data.Context
+{
+    class SqlContext : DbContext
+    {
+        public SqlContext() : base(ConfigurationManager.AppSettings["SqlConnection"])
+        {
+        }
+        static SqlContext()
+        {
+            Database.SetInitializer<SqlContext>(null);
+        }
+        public DbSet<DbUser> Users { get; set; } 
+    }
+}
--- Data/Repositories/Abstraction/PgsqlRepository.cs
+++ Data/Repositories/Abstraction/PgsqlRepository.cs
@@ -1,47 +1,77 @@
-﻿using System;
+﻿using System.Data.Entity;
+using Data.Context;
+using Data.Model;
+using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using Data.Model;
 
 namespace Data.Repositories.Abstraction
 {
     class PgsqlRepository<TModel>: IRepository<TModel> where TModel : DbModel
     {
+        private readonly SqlContext _db;
+
+        public PgsqlRepository()
+        {
+            _db = new SqlContext();
+        }
         public TModel Find(int id)
         {
-            throw new NotImplementedException();
+            return _db.Set<TModel>().Find(id);
         }
 
         public TModel Find(Func<TModel, bool> query)
         {
-            throw new NotImplementedException();
+            return _db.Set<TModel>().Find(query);
         }
 
         public bool Exists(Func<TModel, bool> query)
         {
-            throw new NotImplementedException();
+            return _db.Set<TModel>().Any(query);
         }
 
         public IEnumerable<TModel> GetAll()
         {
-            throw new NotImplementedException();
+            return _db.Set<TModel>().Where(m => m.IsDeleted == false);
         }
 
         public TModel Create(TModel item)
         {
-            throw new NotImplementedException();
+            return _db.Set<TModel>().Add(item);
         }
 
         public bool Update(TModel updatedItem)
         {
-            throw new NotImplementedException();
+            _db.Entry(updatedItem).State = EntityState.Modified;
+            return true;
         }
 
         public bool SoftDelete(int id)
         {
-            throw new NotImplementedException();
+            var itemToDelete = Find(id);
+            if (itemToDelete != null)
+            {
+                itemToDelete.IsDeleted = true;
+                Update(itemToDelete);
+                return true;
+            }
+            return false;
+        }
+
+        public void Dispose()
+        {
+            SaveChanges();
+            _db.Dispose();
+        }
+
+        public bool SaveChanges()
+        {
+            if (_db.ChangeTracker.HasChanges())
+            {
+                _db.SaveChanges();
+                return true;
+            }
+            return false;
         }
     }
 }
